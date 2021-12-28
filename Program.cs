16e76739dd08689c88fcc78c49bcc86e0ee3221f
Alan Hape <ahape@brightmetrics.com>
2021-12-27 16:47:09 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+
+namespace MyNamespace
+{
+    class MyClassCS
+    {
+        static string buildDir = @"c:\src\projects\BrightMetricsWeb\BrightMetricsWeb.BrightMetricsWebUI\UI-2\scripts\build";
+        static string outDir = @"c:\src\azure\BrightmetricsWeb";
+        static string fileName = "brightmetrics.js";
+        static void Main()
+        {
+            using var watcher = new FileSystemWatcher(buildDir);
+
+            watcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 //| NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 //| NotifyFilters.Security
+                                 | NotifyFilters.Size;
+
+            watcher.Created += OnCreated;
+            watcher.Changed += OnChanged;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+            watcher.Error += OnError;
+
+            watcher.Filter = fileName;
+            // watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            Console.WriteLine($"Changed: {e.FullPath} on {DateTime.Now:s}");
+
+            var path1 =Path.Combine(buildDir, fileName) ;
+            var path2 =Path.Combine(outDir, @"UI-2\scripts\build", fileName);
+            File.Copy(path1, path2, true);
+        }
+
+        private static void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            string value = $"Created: {e.FullPath}";
+            Console.WriteLine(value);
+        }
+
+        private static void OnDeleted(object sender, FileSystemEventArgs e) =>
+            Console.WriteLine($"Deleted: {e.FullPath}");
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Renamed:");
+            Console.WriteLine($"    Old: {e.OldFullPath}");
+            Console.WriteLine($"    New: {e.FullPath}");
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private static void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine("Stacktrace:");
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                PrintException(ex.InnerException);
+            }
+        }
+    }
+}
